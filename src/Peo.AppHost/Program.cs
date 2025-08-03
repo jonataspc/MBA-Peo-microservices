886d85ffbf1ddfe9ad6390748c56ffeeacb0e0c3
Jonatas Cruz <jonataspc@gmail.com>
2025-08-03 14:47:09 -0300

    feat: inclusão de BFF e atualização de libs

--- src/Peo.AppHost/Program.cs
+++ src/Peo.AppHost/Program.cs
@@ -1,16 +1,38 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+var rabbitmq = builder.AddRabbitMQ("messaging")
+            .WithDataVolume(isReadOnly: false)
+            .WithManagementPlugin();
 
-var faturamentoSvc = builder.AddProject<Projects.Peo_Faturamento_WebApi>("peo-faturamento-webapi");
+var faturamentoSvc = builder.AddProject<Projects.Peo_Faturamento_WebApi>("peo-faturamento-webapi")
+           .WithReference(rabbitmq)
+           .WaitFor(rabbitmq);
 
-var gestaoAlunosSvc = builder.AddProject<Projects.Peo_GestaoAlunos_WebApi>("peo-gestaoalunos-webapi");
+var gestaoAlunosSvc = builder.AddProject<Projects.Peo_GestaoAlunos_WebApi>("peo-gestaoalunos-webapi")
+           .WithReference(rabbitmq)
+           .WaitFor(rabbitmq);
 
-var gestaoConteudoSvc = builder.AddProject<Projects.Peo_GestaoConteudo_WebApi>("peo-gestaoconteudo-webapi");
+var gestaoConteudoSvc = builder.AddProject<Projects.Peo_GestaoConteudo_WebApi>("peo-gestaoconteudo-webapi")
+           .WithReference(rabbitmq)
+           .WaitFor(rabbitmq);
 
-var identitySvc = builder.AddProject<Projects.Peo_Identity_WebApi>("peo-identity-webapi");
+var identitySvc = builder.AddProject<Projects.Peo_Identity_WebApi>("peo-identity-webapi")
+           .WithReference(rabbitmq)
+           .WaitFor(rabbitmq);
 
+var bff = builder.AddProject<Projects.Peo_Web_Bff>("peo-bff")
+        .WithReference(faturamentoSvc)
+        .WaitFor(faturamentoSvc)
+        .WithReference(gestaoAlunosSvc)
+        .WaitFor(gestaoAlunosSvc)
+        .WithReference(gestaoConteudoSvc)
+        .WaitFor(gestaoConteudoSvc)
+        .WithReference(identitySvc)
+        .WaitFor(identitySvc);
 
-
-builder.AddProject<Projects.Peo_Web_Spa>("peo-frontend");
+builder.AddProject<Projects.Peo_Web_Spa>("peo-frontend")
+       .WithExternalHttpEndpoints()
+       .WithReference(bff)
+       .WaitFor(bff);
 
 builder.Build().Run();
