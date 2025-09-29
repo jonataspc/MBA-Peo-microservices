77295bf8bbbad07aaa1269f410a7859ce38e9456
Jonatas Cruz <jonataspc@gmail.com>
2025-09-28 22:15:16 -0400

    refact: adicão migrations

--- src/Peo.Faturamento.Infra.Data/Migrations/20250929021326_AjustesEstrutura.Designer.cs
+++ src/Peo.Faturamento.Infra.Data/Migrations/20250929021326_AjustesEstrutura.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Peo.Faturamento.Infra.Data.Contexts;
 
@@ -10,9 +11,11 @@ using Peo.Faturamento.Infra.Data.Contexts;
 namespace Peo.Faturamento.Infra.Data.Migrations
 {
     [DbContext(typeof(CobrancaContext))]
-    partial class CobrancaContextModelSnapshot : ModelSnapshot
+    [Migration("20250929021326_AjustesEstrutura")]
+    partial class AjustesEstrutura
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
