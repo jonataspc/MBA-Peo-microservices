1d3e8ac046a35fc945b5adcadb7e530f1a06fc4e
Jonatas Cruz <jonataspc@gmail.com>
2025-12-21 00:26:57 -0300

    fix: correção de bug: cadastro do usuario admin em Usuarios

--- src/Peo.Identity.Infra.Data/Helpers/IdentityDbMigrationHelpers.cs
+++ src/Peo.Identity.Infra.Data/Helpers/IdentityDbMigrationHelpers.cs
@@ -4,6 +4,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Peo.Core.DomainObjects;
+using Peo.Core.Entities;
 using Peo.Identity.Infra.Data.Contexts;
 using System.Diagnostics;
 
@@ -33,11 +34,11 @@ namespace Peo.Identity.Infra.Data.Helpers
                 var context = scope.ServiceProvider.GetRequiredService<IdentityContext>();
 
                 await context.Database.MigrateAsync();
-                await SeedAspNetIdentity(scope.ServiceProvider);
+                await SeedAspNetIdentity(scope.ServiceProvider, context);
             }
         }
 
-        private static async Task SeedAspNetIdentity(IServiceProvider serviceProvider)
+        private static async Task SeedAspNetIdentity(IServiceProvider serviceProvider, IdentityContext context)
         {
             var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
 
@@ -58,10 +59,10 @@ namespace Peo.Identity.Infra.Data.Helpers
                 Debug.WriteLine(e);
             }
 
-            await AddAdminUserAsync(serviceProvider);
+            await AddAdminUserAsync(serviceProvider, context);
         }
 
-        private static async Task AddAdminUserAsync(IServiceProvider serviceProvider)
+        private static async Task AddAdminUserAsync(IServiceProvider serviceProvider, IdentityContext context)
         {
             var userManager = serviceProvider.GetRequiredService<UserManager<IdentityUser>>();
 
@@ -76,6 +77,8 @@ namespace Peo.Identity.Infra.Data.Helpers
                 return;
             }
 
+            // ONLY FOR DEMO PURPOSES
+            // CHANGE THE PASSWORD BEFORE DEPLOYING TO PRODUCTION !!!
             var result = await userManager.CreateAsync(adminUser, "@dmin!");
 
             if (result.Succeeded)
@@ -83,6 +86,9 @@ namespace Peo.Identity.Infra.Data.Helpers
                 await userManager.ConfirmEmailAsync(adminUser, await userManager.GenerateEmailConfirmationTokenAsync(adminUser));
                 await userManager.AddToRoleAsync(adminUser, AccessRoles.Aluno);
                 await userManager.AddToRoleAsync(adminUser, AccessRoles.Admin);
+
+                context.ApplicationUsers.Add(new Usuario(Guid.Parse(adminUser.Id), "Administrador do Sistema", adminUser.Email));
+                await context.SaveChangesAsync();
             }
         }
     }
